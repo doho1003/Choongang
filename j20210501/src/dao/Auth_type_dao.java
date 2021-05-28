f3e5cb8ff6503e57f21f6d404c9c26c14f519278
doho1003 <50233733+doho1003@users.noreply.github.com>
2021-05-28 16:52:22 +0900

    Add files via upload

--- j20210501/src/dao/Auth_type_dao.java
+++ j20210501/src/dao/Auth_type_dao.java
@@ -36,38 +36,4 @@ public class Auth_type_dao {
 
 		return conn;
 	}
-
-	public Auth_type showAll() throws SQLException {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		Auth_type auth_type = new Auth_type();
-
-		String sql = "SELECT * FROM auth_type";
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				auth_type.setAuth_no(rs.getInt("auth_no"));
-				auth_type.setAuth_name(rs.getString("auth_name"));
-				auth_type.setAuth_enroll(rs.getString("auth_enroll"));
-				auth_type.setAuth_modify(rs.getString("auth_modify"));
-				auth_type.setAuth_delete(rs.getString("auth_delete"));
-				auth_type.setAuth_inquiry(rs.getString("auth_inquiry"));
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-			if (rs != null)
-				rs.close();
-			if (pstmt != null)
-				pstmt.close();
-			if (conn != null)
-				conn.close();
-		}
-
-		return auth_type;
-	}
 }
