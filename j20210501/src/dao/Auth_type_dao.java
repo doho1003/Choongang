c5f1da1717639e7f4598df446bb927ef8449ebc0
doho1003 <50233733+doho1003@users.noreply.github.com>
2021-05-28 16:52:53 +0900

    Add files via upload

--- j20210501/src/dao/Auth_type_dao.java
+++ j20210501/src/dao/Auth_type_dao.java
@@ -1,9 +1,6 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
